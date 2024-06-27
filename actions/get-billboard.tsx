2182a5ce79ce5e347e2aa5ea6bf06aa9efc6fa57
Martin <martinp@dubium.no>
2024-06-27 10:53:36 +0200

    update

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -2,8 +2,11 @@ import { Billboard } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
-const getBillboard = async (id: string): Promise<Billboard[]> => {
-    const res = await fetch(`${URL}/${id}`)
+const getBillboard = async (id: string): Promise<Billboard> => {
+    const res = await fetch(`${URL}/${id}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch billboard');
+    }
     return res.json();
 };
 
